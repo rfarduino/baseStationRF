d80ce45bbaa22bee93700b9c6b1f51201a297a6b
lovejoy777 <salovejoy777@gmail.com>
2016-02-29 15:13:53 +0000

    added full setup mode and normal mode, need a swith between pin 8 and 9. for selecting between modes. switch on is setup switch off is normal.

--- android-ethernet-RF-Trans.ino
+++ android-ethernet-RF-Trans.ino
@@ -3,83 +3,121 @@ Written by Steve Lovejoy aka lovejoy777
 This code is for the use with ethernet/rf base station
 to control rf receiver relays.
 */
-#include <RCSwitch.h>    // needed to sent rf code
-#include <SPI.h>         // needed for Arduino versions later than 0018
 #include <Ethernet.h>
-#include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
+#include <EthernetUdp.h>
+
 #include <SoftwareSerial.h>
+#include <RCSwitch.h>
+#include <SD.h>
+#include <SPI.h>
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = {0x90,0xA2,0xDA,0x0D,0x8B,0x8F};
-IPAddress ip(192, 168, 0, 105);
+byte mac[] = {
+  0x90, 0xA2, 0xDA, 0x0D, 0x8B, 0x8F
+};
+IPAddress ip(192, 168, 0, 136);
 EthernetServer server(8032);
-unsigned int localPort = 8032; 
+File myFile;
+unsigned int localPort = 8032;
 boolean incoming = 0;
-// local port to listen on
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+char  ReplyBuffer[] = "acknowledged";
+int pin8 = 8;
+int pin9 = 9;
+int setupMode = 0;
+byte ip1[] = {192, 168, 0, 107};
+String strIP;
+String BTdata;
 
-// buffers for receiving and sending data
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
-char  ReplyBuffer[] = "acknowledged";       // a string to send back
-
-// An EthernetUDP instance to let us send and receive packets over UDP
+//SoftwareSerial mySerial(2, 3); // RX, TX
+RCSwitch mySwitch = RCSwitch();
 EthernetUDP Udp;
 
-RCSwitch mySwitch = RCSwitch();
-
 void setup() {
-  // start the Ethernet and UDP:
-  Ethernet.begin(mac,ip);
-  Udp.begin(localPort);
+  
+  pinMode(pin8, OUTPUT);
+  pinMode(pin9, INPUT);
+  digitalWrite (pin8, HIGH);
+  mySwitch.enableTransmit(7);
+  strIP = "192.168.0.105";
+  
   Serial.begin(9600);
-  // RF Transmitter data pin is connected to Arduino Pin #4  
-  mySwitch.enableTransmit(4);
+  while (!Serial) {
+    ;
+  }
+  Serial.println("Initializing SD card...");
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  
+  // SETUP MODE
+  setupMode = digitalRead(pin9);
+  if (setupMode == HIGH) {
+    while (Serial.available() == 0) { // if the data came
+    }
+    BTdata = Serial.readString(); // read byte
+    Serial.println(BTdata);
+    Serial.println("SETUP MODE");
+    if (SD.exists("ipa.txt")) {
+      // delete the file:
+      SD.remove("ipa.txt");
+    } else {
+      Serial.println("ipa.txt doesn't exist.");
+    }
+    myFile = SD.open("ipa.txt", FILE_WRITE);
+    if (myFile) {
+      myFile.println(BTdata); // write to sdcard
+      myFile.close();
+      Serial.println("done.");
+    } else {
+      Serial.println("error opening ipa.txt");
+    }
+    myFile = SD.open("ipa.txt");
+    if (myFile) {
+      while (myFile.available()) {
+        String strIP = myFile.readStringUntil('\n');
+        char buf[16];
+        strIP.toCharArray(buf, 15);
+        sscanf(buf, "%d.%d.%d.%d", &ip1[0], &ip1[1], &ip1[2], &ip1[3]);
+      }
+      myFile.close();
+    } else {
+      Serial.println("error opening ipa.txt");
+    }
+  } else {
+    
+    // USER MODE
+    Serial.println("USER MODE");
+    myFile = SD.open("ipa.txt");
+    if (myFile) {
+      while (myFile.available()) {
+        String strIP = myFile.readStringUntil('\n');
+        char buf[16];
+        strIP.toCharArray(buf, 15);
+        sscanf(buf, "%d.%d.%d.%d", &ip1[0], &ip1[1], &ip1[2], &ip1[3]);
+      }
+      myFile.close();
+    } else {
+      Serial.println("error opening ipa.txt");
+    }
+  }
+  
+  // START ETHERNET
+  Ethernet.begin(mac, ip1);
+  Udp.begin(localPort);
 }
 
 void loop() {
-  
-// if there's data available, read a packet
   int packetSize = Udp.parsePacket();
-
-Serial.println(packetSize);
-if(packetSize)
+  if (packetSize)
   {
-    Serial.print("Received packet of size ");
-    Serial.println(packetSize);
-    Serial.print("From ");
-    IPAddress remote = Udp.remoteIP();
-    for (int i =0; i < 4; i++)
-    {
-      Serial.print(remote[i], DEC);
-      if (i < 3)
-      {
-        Serial.print(".");
-      }
+    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    int i = atoi(&packetBuffer[0]);
+    if (i > 0) {
+      mySwitch.send(i, 24);
     }
-    Serial.print(", port ");
-    Serial.println(Udp.remotePort());
-
-    // read the packet into packetBufffer
-    Udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
-    Serial.println("Contents:");
-    Serial.println(packetBuffer);
-    Serial.println(packetBuffer[0]);
-
-  // function, if packet data from ethernet is 1 send 5392 with RF
-    if(packetBuffer[0]=='1'){
-     mySwitch.send(5392, 24);
-     
-  // function, if packet data from ethernet is 2 send 5393 with RF    
+    else {
+      Serial.println("Unknown Code");
     }
-    else if(packetBuffer[0]=='2')
-    {
-      mySwitch.send(5393, 24);
-
-    }
-
-    // send a reply, to the IP address and port that sent us the packet we received
-    Udp.beginPacket(Udp.remoteIP(),Udp.remotePort());
-    Udp.write("Received");
-    Udp.endPacket();
   }
 }
